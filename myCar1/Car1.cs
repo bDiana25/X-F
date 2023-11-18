4b88f7a17d406264c622c519d8f67c44fabbe28c
bDiana25 <diana.bejan@cncn.ro>
2023-11-18 14:15:22 +0200

    new commit

---
+++ myCar1/Car1.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace myCar1
+{
+    class Car1
+    {
+        public string color;
+        public string name;
+        public int maxspeed;
+        public int year;
+    }
+    public Car1(string nameName, string colorName, int maxspeedValue, int yearValue)
+    {
+        name = nameName;
+        color = colorName;
+        year = yearValue;
+        maxSpeed = maxspeedValue;
+    }
+    public void fullThrottle()
+    {
+        Console.WriteLine(name + "is going at the speed of" + maxSpeed);
+    }
+
+
+
+}
