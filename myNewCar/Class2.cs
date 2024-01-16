f2e5471b8cb8aee49a3bb2d09b4ee08dfecba3f4
bDiana25 <diana.bejan@cncn.ro>
2024-01-16 19:21:33 +0200

    new commit

---
+++ myNewCar/Class2.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace myNewCar
+{
+    internal class Class2
+    {
+    }
+}
